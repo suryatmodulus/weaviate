703c8c9979b1862119b33bd75c68965792059174
Marcin Antas <antas.marcin@gmail.com>
2023-03-23 17:50:45 +0100

    Add e2e tests

--- test/acceptance/graphql_resolvers/local_get_with_group_by_test.go
+++ test/acceptance/graphql_resolvers/local_get_with_group_by_test.go
@@ -15,6 +15,7 @@ import (
 	"testing"
 
 	graphqlhelper "github.com/weaviate/weaviate/test/helper/graphql"
+	"github.com/weaviate/weaviate/test/helper/sample-schema/documents"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -22,6 +23,21 @@ import (
 )
 
 func groupByObjects(t *testing.T) {
+	getGroup := func(value interface{}) map[string]interface{} {
+		group := value.(map[string]interface{})["_additional"].(map[string]interface{})["group"].(map[string]interface{})
+		return group
+	}
+
+	getGroupHits := func(group map[string]interface{}) []string {
+		result := []string{}
+		hits := group["hits"].([]interface{})
+		for _, hit := range hits {
+			content := hit.(map[string]interface{})["content"]
+			result = append(result, content.(string))
+		}
+		return result
+	}
+
 	t.Run("group by: people by city", func(t *testing.T) {
 		query := `
 		{
@@ -36,7 +52,6 @@ func groupByObjects(t *testing.T) {
 						objectsPerGroup: 10
 					}
 				){
-					name
 					_additional{
 						id
 						group{
@@ -55,26 +70,10 @@ func groupByObjects(t *testing.T) {
 			}
 		}
 		`
-
 		result := graphqlhelper.AssertGraphQL(t, helper.RootAuth, query)
 		groups := result.Get("Get", "Person").AsSlice()
 
 		require.Len(t, groups, 3)
-
-		getGroup := func(value interface{}) map[string]interface{} {
-			group := value.(map[string]interface{})["_additional"].(map[string]interface{})["group"].(map[string]interface{})
-			return group
-		}
-
-		getGroupHits := func(group map[string]interface{}) []string {
-			result := []string{}
-			hits := group["hits"].([]interface{})
-			for _, hit := range hits {
-				content := hit.(map[string]interface{})["content"]
-				result = append(result, content.(string))
-			}
-			return result
-		}
 
 		expectedGroup1 := []string{
 			"8615585a-2960-482d-b19d-8bee98ade52c",
@@ -101,6 +100,110 @@ func groupByObjects(t *testing.T) {
 			} else if len(ids) == 1 {
 				assert.ElementsMatch(t, expectedGroup3, ids)
 			}
+		}
+	})
+
+	t.Run("group by: passages by documents", func(t *testing.T) {
+		create := func(t *testing.T, multishard bool) {
+			for _, class := range documents.ClassesContextionaryVectorizer(multishard) {
+				createObjectClass(t, class)
+			}
+			for _, obj := range documents.Objects() {
+				helper.CreateObject(t, obj)
+				helper.AssertGetObjectEventually(t, obj.Class, obj.ID)
+			}
+		}
+		groupBy := func(t *testing.T) {
+			query := `
+			{
+				Get{
+					Passage(
+						nearObject:{
+							id: "00000000-0000-0000-0000-000000000001" 
+						}
+						groupBy:{
+							path:["ofDocument"]
+							groups:2
+							objectsPerGroup: 10
+						}
+					){
+						_additional{
+							id
+							group{
+								count
+								maxDistance
+								minDistance
+								hits {
+									content
+									_additional{
+										distance
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			`
+			result := graphqlhelper.AssertGraphQL(t, helper.RootAuth, query)
+			groups := result.Get("Get", "Passage").AsSlice()
+
+			require.Len(t, groups, 3)
+
+			expectedGroup1 := []string{
+				documents.PassageIDs[0].String(),
+				documents.PassageIDs[1].String(),
+				documents.PassageIDs[2].String(),
+				documents.PassageIDs[3].String(),
+				documents.PassageIDs[4].String(),
+				documents.PassageIDs[5].String(),
+			}
+
+			expectedGroup2 := []string{
+				documents.PassageIDs[6].String(),
+				documents.PassageIDs[7].String(),
+			}
+
+			for _, current := range groups {
+				group := getGroup(current)
+				ids := getGroupHits(group)
+				if len(ids) == 3 {
+					assert.ElementsMatch(t, expectedGroup1, ids)
+				} else if len(ids) == 2 {
+					assert.ElementsMatch(t, expectedGroup2, ids)
+				}
+			}
+		}
+		delete := func(t *testing.T) {
+			deleteObjectClass(t, documents.Passage)
+			deleteObjectClass(t, documents.Document)
+		}
+
+		tests := []struct {
+			name       string
+			multishard bool
+		}{
+			{
+				name:       "single shard",
+				multishard: false,
+			},
+			{
+				name:       "multi shard",
+				multishard: true,
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				t.Run("create", func(t *testing.T) {
+					create(t, tt.multishard)
+				})
+				t.Run("group by", func(t *testing.T) {
+					groupBy(t)
+				})
+				t.Run("delete", func(t *testing.T) {
+					delete(t)
+				})
+			})
 		}
 	})
 }
